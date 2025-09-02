29dc5a163822cf1a88ea8b0fd4fd3b18302c86c5
Vandana <vandanasharma.sharma456@gmail.com>
2025-09-03 00:24:54 +0530

    Initial clean commit of all project files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vandana Sharma",
+    author_email="vandanasharma.sharma456@gmail.com",
+    packages=find_packages()
+)
